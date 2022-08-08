3bf5cf7043c7890d71bd918ac3a7730a5a4b8f8b
mohanasaik <mohanasai.k@gmail.com>
2022-08-07 22:58:45 -0600

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,10 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+import './style.scss';
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
